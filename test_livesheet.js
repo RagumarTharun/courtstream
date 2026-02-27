72b9830a93a0e57f45d49a79e1468d9101067cc2
RagumarTharun <ragumartharun2005@gmail.com>
2026-02-27 15:15:10 +0530

    update port in test_livesheet.js

--- test_livesheet.js
+++ test_livesheet.js
@@ -6,7 +6,7 @@ const puppeteer = require('puppeteer');
   page.on('console', msg => console.log('PAGE LOG:', msg.text()));
   page.on('pageerror', error => console.log('PAGE ERROR:', error.message));
 
-  await page.goto('http://localhost:8080/scorekeeper.html', { waitUntil: 'networkidle0' });
+  await page.goto('http://localhost:3000/scorekeeper.html', { waitUntil: 'networkidle0' });
 
   // Set up match
   await page.click('input[value="FIBA"]');
